2ee5bef2777fe3494a5e5b778662c768ff4815ea
Martijn <m_kopie@hotmail.com>
2019-11-23 10:56:29 +0100

    QVTKOpenGLWidget -> QVTKOpenGLNativeWidget

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 #include <QApplication>
 #include <QSurfaceFormat>
 
-#include <QVTKOpenGLWidget.h>
+#include <QVTKOpenGLNativeWidget.h>
 
 int main(int argc, char *argv[])
 {
-  QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());
+  QSurfaceFormat::setDefaultFormat(QVTKOpenGLNativeWidget::defaultFormat());
 
   QApplication a(argc, argv);
   MainWindow w;
